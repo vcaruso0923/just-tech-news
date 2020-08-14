da31e3a2a3bed7daf07c97e4546f5039bd7048d9
vcaruso <vinnycar0923@gmail.com>
2020-08-14 12:58:13 -0700

    added login route with hashed pword

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,11 +6,11 @@ router.get('/', (req, res) => {
     // Access our User model and run .findAll() method)
     User.findAll({
         attributes: { exclude: ['password'] }
-      })
+    })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
+            console.log(err);
+            res.status(500).json(err);
         });
 });
 
@@ -48,6 +48,29 @@ router.post('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+//login route
+router.post('/login', (req, res) => {
+    // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
 });
 
 // PUT /api/users/1

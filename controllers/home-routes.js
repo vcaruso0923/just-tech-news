873550d8773ade4594ca4d12033ebc12371cef0d
vcaruso <vinnycar0923@gmail.com>
2020-08-19 11:35:38 -0700

    Imlemented signup and login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-
+//get for homepage
 router.get('/', (req, res) => {
+    console.log(req.session);
+
     Post.findAll({
         attributes: [
             'id',
@@ -35,6 +37,15 @@ router.get('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+//get for login
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
 });
 
 module.exports = router;
